f16c56692783369c822bebef4eb1b975c2b8abf3
Kirsten Korevaar <kirsten@eml.cc>
2021-06-16 14:53:33 +1000

    Add BookList component

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -1,6 +1,19 @@
-describe("Bookish Application", function () {
-  it("visits the Bookish website", function () {
+/* eslint-disable jest/valid-expect */
+
+describe("Bookish Application", () => {
+  it("visits the Bookish website", () => {
     cy.visit("http://localhost:3000");
     cy.get('h1[data-test="heading"]').contains("Bookish");
+  });
+  it("shows a book list", () => {
+    cy.visit("http://localhost:3000");
+    cy.get('div[data-test="book-list"]').should("exist");
+    cy.get('div[data-test="book-item"]').should((books) => {
+      expect(books).to.have.length(2);
+      const titles = [...books].map(
+        (book) => book.querySelector("h2").innerHTML
+      );
+      expect(titles).to.deep.equal(["Refactoring", "Domain-driven design"]);
+    });
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
+import Booklist from "./BookList";
 
 function App() {
+  const books = [{ name: "Refactoring" }, { name: "Domain-driven design" }];
+
   return (
     <div className="App">
       <Typography variant="h1" component="h1" data-test="heading">
-        Bookish!
+        Bookish
       </Typography>
+      <Booklist books={books} />
     </div>
   );
 }
