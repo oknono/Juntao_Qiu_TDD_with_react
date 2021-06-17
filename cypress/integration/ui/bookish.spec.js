6d2f5b4183397585839e2ab9d831ca185d8ecb1b
Kirsten Korevaar <kirsten@eml.cc>
2021-06-18 00:01:15 +1000

    Refactor cypress test for reuseability

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -8,40 +8,61 @@ describe("Bookish Application", () => {
       .catch((err) => err);
   });
 
+  beforeEach(() => {
+    goToPage();
+  });
+
   it("visits the Bookish website", () => {
-    cy.visit("http://localhost:3000");
-    cy.get('h1[data-test="heading"]').contains("Bookish");
+    checkPageTitle();
   });
 
   it("shows a book list", () => {
-    cy.visit("http://localhost:3000");
-    cy.get('div[data-test="book-list"]').should("exist");
-    cy.get("div.book-item").should((books) => {
-      expect(books).to.have.length(4);
-      const titles = [...books].map(
-        (book) => book.querySelector("h2").innerHTML
-      );
-      expect(titles).to.deep.equal([
-        "Refactoring",
-        "Domain-driven design",
-        "Building Microservices",
-        "Acceptance Test Driven Development with React",
-      ]);
-    });
+    checkBookListWithTitles([
+      "Refactoring",
+      "Domain-driven design",
+      "Building Microservices",
+      "Acceptance Test Driven Development with React",
+    ]);
   });
 
   it("goes to book details page", () => {
-    cy.visit("http://localhost:3000");
-    cy.get("div.book-item").contains("View Details").eq(0).click();
+    selectNthBookInList(0);
     cy.url().should("include", "/books/1");
     cy.get("h2.book-title").contains("Refactoring");
   });
 
   it("searches for a title", () => {
-    cy.visit("http://localhost:3000");
-    cy.get("div.book-item").should("have.length", 4);
-    cy.get('[data-test="search"] input').type("design");
-    cy.get("div.book-item").should("have.length", 1);
-    cy.get("div.book-item").eq(0).contains("Domain-driven design");
+    checkBookListWithTitles([
+      "Refactoring",
+      "Domain-driven design",
+      "Building Microservices",
+      "Acceptance Test Driven Development with React",
+    ]);
+    performSearch("design");
+    checkBookListWithTitles(["Domain-driven design"]);
   });
 });
+
+const goToPage = () => {
+  cy.visit("http://localhost:3000");
+};
+
+const checkPageTitle = () => {
+  cy.get('h1[data-test="heading"]').contains("Bookish");
+};
+
+const checkBookListWithTitles = (expectation = []) => {
+  cy.get('div[data-test="book-list"]').should("exist");
+  cy.get("div.book-item").should((books) => {
+    expect(books).to.have.length(expectation.length);
+    const titles = [...books].map((x) => x.querySelector("h2").innerHTML);
+    expect(titles).to.deep.equal(expectation);
+  });
+};
+
+const selectNthBookInList = (n) => {
+  cy.get("div.book-item").contains("View Details").eq(n).click();
+};
+const performSearch = (search) => {
+  cy.get('[data-test="search"] input').type(search);
+};
